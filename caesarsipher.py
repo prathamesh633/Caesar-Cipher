676eb2c8b477b538a38c259cea562a71bfa28e77
Prathamesh Prakash Bhujade <129214303+prathamesh633@users.noreply.github.com>
2024-02-21 22:37:39 +0530

    Create caesarsipher.py

---
+++ caesarsipher.py
@@ -0,0 +1,40 @@
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+from art import logo
+
+print(logo)
+game = True
+while game:
+
+    def caesar(start_text, shift_amount, cipher_direction):
+        end_text = ""
+        if cipher_direction == "decode":
+            if shift_amount > len(alphabet):
+                shift_amount %= len(alphabet)
+            shift_amount *= -1
+        for char in start_text:
+
+            if char not in alphabet:
+                end_text += char
+            else:
+                position = alphabet.index(char)
+                new_position = position + shift_amount
+                end_text += alphabet[new_position]
+
+        print(f"Here's the {cipher_direction}d result: {end_text}")
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    repeat_ans = input(
+        "Type 'yes' if you want to go again. Otherwise type 'no'")
+    if repeat_ans == "yes":
+        complete = True
+    else:
+        print("game over")
+        complete = False
